c9b508d5730960489707e32e1ed6d5c8d81042d8
Nastia Bzova <bzovanastia@gmail.com>
2025-09-19 12:43:21 +0200

    created Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function SiteName() {
+    return (
+        <header className="text-center py-8 px-4">
+         <h1 className="text-5xl md:text-6xl font-serif font-bold text-amber-900 tracking-wide border-b-2 border-amber-700 pb-4 inline-block">
+            Books library
+        </h1>
+        </header>
+    )
+}
