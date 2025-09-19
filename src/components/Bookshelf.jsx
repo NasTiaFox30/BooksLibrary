6126641ed76e883c35ace6a632d1e719acff0a00
Nastia Bzova <bzovanastia@gmail.com>
2025-09-19 12:52:50 +0200

    Bookshelf component

---
+++ src/components/Bookshelf.jsx
@@ -0,0 +1,30 @@
+import Shelf from './Shelf';
+import { shelves } from '../data/shelves';
+
+export default function Bookshelf({ books }) {
+  return (
+    <div className="max-w-6xl mx-auto px-4">
+      {/* Контейнер шафи */}
+      <div className="bg-amber-800 rounded-lg shadow-2xl p-6 md:p-8 relative">
+        {/* Верх шафи */}
+        <div className="h-4 bg-amber-900 rounded-t-lg mb-2"></div>
+        
+        {/* Полиці з книгами */}
+        {shelves.map((shelf, shelfIndex) => (
+          <Shelf 
+            key={shelfIndex} 
+            shelf={shelf} 
+            shelfIndex={shelfIndex} 
+            books={books} 
+          />
+        ))}
+        
+        {/* Ніжки шафи */}
+        <div className="flex justify-between mt-4">
+          <div className="w-12 h-16 bg-amber-900 rounded-t-md"></div>
+          <div className="w-12 h-16 bg-amber-900 rounded-t-md"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
