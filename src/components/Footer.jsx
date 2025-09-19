e4f54a6b6bcd5177f1ad4e3bd2b9fa9d3f8d51fd
Nastia Bzova <bzovanastia@gmail.com>
2025-09-19 12:53:02 +0200

    Create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="py-4 text-center text-amber-900 text-sm">
+        <p>© 2025 Anastasiia Bzova. Усі права захищені.</p>
+      </footer>
+    )
+}
