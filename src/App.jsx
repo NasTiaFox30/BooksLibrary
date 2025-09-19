58ccde00654ada382059f01259758ec4064b02ee
Nastia Bzova <bzovanastia@gmail.com>
2025-09-19 11:38:14 +0200

    App changed struture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import MainScreen from "./pages/MainScreen";
+
 export default function App() {
 
   return (
-    <>
-      Test
-    </>
-  )
+    <div className="min-h-screen bg-amber-100 flex flex-col items-center py-10">
+      
+      <MainScreen/>
+      
+    </div>
+  );
 }
