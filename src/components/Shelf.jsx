488c52e4316fd75ed811d0a5e4f3808cbc1d334f
Nastia Bzova <bzovanastia@gmail.com>
2025-09-19 22:19:13 +0200

    added event onBookClick; checking length of shelfbooks

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -1,22 +1,26 @@
 import Book from './Book';
 
-export default function Shelf({ shelf, shelfIndex, books }) {
+export default function Shelf({ shelf, shelfIndex, books, onBookClick }) {
   const shelfBooks = books.filter(book => book.shelf === shelfIndex);
+  if (!shelfBooks || shelfBooks.length === 0) {return null;}
 
   return (
     <div className="mb-10 last:mb-0 relative">
-      {/* Сама полиця */}
+      {/* Shelf */}
       <div className="h-6 bg-amber-700 rounded-md shadow-md relative">
         <div className="absolute top-0 left-4 text-amber-200 text-sm font-semibold pt-1">
           {shelf.genre}
         </div>
       </div>
       
-      {/* Простір для книг на полиці */}
+      {/* Space for books */}
       <div className="h-32 bg-amber-600 rounded-b-md relative -mt-1 pt-2 px-2">
-        {/* Книги на цій полиці */}
         {shelfBooks.map(book => (
-          <Book key={book.id} book={book} />
+          <Book 
+            key={book.id} 
+            book={book} 
+            onBookClick={onBookClick}
+          />
         ))}
       </div>
     </div>
