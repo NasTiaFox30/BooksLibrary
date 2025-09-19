e5101609f4379f67bc97f4415ffcb1f011c4a9c8
Nastia Bzova <bzovanastia@gmail.com>
2025-09-19 22:00:20 +0200

    changed design construction + added onBook event click

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,29 +1,49 @@
 import Shelf from './Shelf';
-import { shelves } from '../data/shelves';
 
-export default function Bookshelf({ books }) {
+export default function Bookshelf({ shelves, books, onBookClick }) {
+  console.log('Bookshelf props:', { shelves, books });
+  
+  if (!shelves || shelves.length === 0) {
+    return (
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="text-center text-amber-900 text-lg py-12">
+          Немає полиць для відображення
+        </div>
+      </div>
+    );
+  }
+  if (!books || books.length === 0) {
+    return (
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="text-center text-amber-900 text-lg py-12">
+          Немає книг для відображення
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-6xl mx-auto px-4">
-      {/* Верх шафи */}
-      <div className="h-10 bg-amber-900 rounded-t-lg"></div>
-      {/* Контейнер шафи */}
-      <div className="bg-amber-800 rounded-lg shadow-2xl p-6 md:p-8 relative">
-        
-        {/* Полиці з книгами */}
+      {/* Top */}
+      <div className="h-10 bg-cyan-200 rounded-t-lg"></div>
+      {/* Container */}
+      <div className="text-cyan-200 p-6 md:p-8 relative">
+        {/* Shelves */}
         {shelves.map((shelf, shelfIndex) => (
           <Shelf 
             key={shelfIndex} 
             shelf={shelf} 
             shelfIndex={shelfIndex} 
             books={books} 
+            onBookClick={onBookClick}
           />
         ))}
-        
-        {/* Ніжки шафи */}
-        <div className="flex justify-between mt-4">
-          <div className="w-12 h-16 bg-amber-900 rounded-t-md"></div>
-          <div className="w-12 h-16 bg-amber-900 rounded-t-md"></div>
-        </div>
+      </div>
+
+      {/* Bottom */}
+      <div className='shadow-2xl'>
+        <div className='h-8 bg-cyan-400'></div>
+      <div className='h-16 bg-cyan-200 rounded-b-md'></div>
       </div>
     </div>
   );
