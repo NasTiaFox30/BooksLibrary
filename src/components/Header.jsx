43cf7c63af580e5c2a6543e70bbc52677d8ca50d
Nastia Bzova <bzovanastia@gmail.com>
2025-09-23 00:55:18 +0200

    UPDATE header: new onTitleClick function + OnNavigate, currentScreen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-export default function SiteName() {
-    return (
-        <header className="text-center py-8 px-4">
-         <h1 className="text-5xl md:text-6xl font-bold pb-4 inline-block">
-            Books library
-        </h1>
-        </header>
-    )
+export default function Header({ currentScreen, onNavigate, onTitleClick }) {
+  const handleLogoClick = () => {
+    if (currentScreen !== 'home') {
+      onNavigate('home');
+    } else {
+      // on Home page
+      onTitleClick();
+    }
+  };
+
+  return (
+    <header className="text-center py-8 px-4">
+      <h1 
+        className="text-5xl md:text-6xl font-bold pb-4 inline-block cursor-pointer select-none hover:opacity-80 transition-opacity active:scale-95"
+        onClick={handleLogoClick}
+      >Books library
+      </h1>
+    </header>
+  );
 }
