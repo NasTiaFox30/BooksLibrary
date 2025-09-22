1e2ff51b78829aba1c86c7bc25710816353a7ea4
Nastia Bzova <bzovanastia@gmail.com>
2025-09-22 14:59:43 +0200

    updates textures for shelf

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -9,34 +9,26 @@ export default function Shelf({ shelf, shelfIndex, books, onBookClick }) {
       {/* Shelf and Genre Label Container */}
       <div className="relative flex justify-center">
         {/* Shelf */}
+        {/* Genre Label - Fixed size and centered */}
         <div
-          className="w-full h-9 relative"
+          className="absolute z-1 -top-4 w-35 h-18 flex items-center justify-center text-center text-black text-xl font-semibold px-2 py-1"
           style={{
-            backgroundRepeat: 'no-repeat',
-            borderTop: '3px solid #3d424c',
-            borderBottom: '3px solid #3d424c'
-          }}
-        ></div>
-
-        {/* Genre Label - Absolutely positioned and centered */}
-        <div
-          className="absolute h-15 z-1 top-3 text-black text-2xl font-semibold px-2 py-1"
-          style={{
-            backgroundImage: 'url(textures/wood-texture.png)',
-            backgroundSize: '100%',
+            backgroundImage: 'url(textures/wood-sign.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
           }}
         >
           {shelf.name}
         </div>
       </div>
-      
+
       {/* Space for books */}
       <div
-        className="h-40 rounded-b-md relative -mt-1 pt-2 px-2"
+        className="h-40 relative -mt-1 pt-2 px-2"
         style={{
           backgroundImage: 'url(textures/shelf-texture.png)',
-          backgroundSize: '100%',
+          backgroundSize: '100% 100%',
           backgroundRepeat: 'no-repeat',
           borderLeft: '3px solid #3d424c',
           borderRight: '3px solid #3d424c',
