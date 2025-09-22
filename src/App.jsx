bfcd387607c6fcfdbbca1cb6a78220eb610785ac
Nastia Bzova <bzovanastia@gmail.com>
2025-09-23 00:52:56 +0200

    import useEfect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Header from "./components/Header"
+import { useState } from 'react';
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./pages/HomeScreen";
 import BookScreen from "./pages/BookScreen";
