8818c8426a9c7d179c060cedcebcb459e041be10
Nastia Bzova <bzovanastia@gmail.com>
2025-09-22 18:11:30 +0200

    changed font for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function SiteName() {
     return (
         <header className="text-center py-8 px-4">
-         <h1 className="text-5xl md:text-6xl font-serif font-bold border-b-2 pb-4 inline-block">
+         <h1 className="text-5xl md:text-6xl font-bold pb-4 inline-block">
             Books library
         </h1>
         </header>
