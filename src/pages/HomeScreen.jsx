9520be70647d19dfb282699099c92ea255739d99
Nastia Bzova <bzovanastia@gmail.com>
2025-09-22 12:39:43 +0200

    Created load Home page function

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -4,11 +4,39 @@ import { GenerateBooks } from "../components/Tools";
 
 export default function HomeScreen() {
   const [books, setBooks] = useState([]);
+  const [shelves, setShelves] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Generate Books
   useEffect(() => {
-    GenerateBooks(setBooks, 20);
+    loadBooks();
   }, []);
+
+  const loadBooks = async () => {
+    try {
+      setLoading(true);
+      // books
+      const { books: generatedBooks, genres } = await GenerateBooks(20);
+      setBooks(generatedBooks);
+
+      // shelves
+      const shelvesData = genres.map(genre => ({ id: genre.id, name: genre.name }));
+      setShelves(shelvesData);
+
+    } catch (error) {
+      console.error('Помилка завантаження:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="text-amber-900 text-xl">Завантаження книг...</div>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
