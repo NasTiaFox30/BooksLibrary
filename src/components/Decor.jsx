e8d1edf56eeace953ee664b5523262396d77ff3d
Nastia Bzova <bzovanastia@gmail.com>
2025-09-23 23:39:07 +0200

    created Decor.jsx

---
+++ src/components/Decor.jsx
@@ -0,0 +1,18 @@
+export function Tape({ degree, size, top, left, bottom, right }) {
+  return (
+    <div
+      className="absolute z-10 opacity-80 flex items-center justify-center"
+      style={{
+        height: size !== null ? size + "px" : "50px",
+        width: size !== null ? size + "px" : "50px",
+        transform: `rotate(${degree}deg)`,
+        top: top !== null ? `${top}px` : 'auto',
+        left: left !== null ? `${left}px` : 'auto',
+        bottom: bottom !== null ? `${bottom}px` : 'auto',
+        right: right !== null ? `${right}px` : 'auto',
+      }}
+    >
+      <img src="/textures/tape.png" alt="tape" />
+    </div>
+  );
+}
