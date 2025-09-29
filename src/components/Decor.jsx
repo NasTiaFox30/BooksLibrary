8bc5684a0dc6af1ad376606cf4e30091f49c1f0c
Nastia Bzova <bzovanastia@gmail.com>
2025-09-30 01:52:01 +0200

    made Decor select-none pointer-events-none

--- src/components/Decor.jsx
+++ src/components/Decor.jsx
@@ -1,7 +1,7 @@
 export function Tape({ degree, size, top, left, bottom, right }) {
   return (
     <div
-      className="absolute z-10 opacity-80 flex items-center justify-center"
+      className="absolute z-10 opacity-80 flex items-center justify-center select-none pointer-events-none"
       style={{
         height: size !== null ? size + "px" : "50px",
         width: size !== null ? size + "px" : "50px",
