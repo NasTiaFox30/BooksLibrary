5354a15b1662a64ab4a66b14f5d4216037b34d90
Nastia Bzova <bzovanastia@gmail.com>
2025-09-30 02:24:49 +0200

    SearchSection mobile adaptation

--- src/components/CollectionScreen/search/SerchSection.jsx
+++ src/components/CollectionScreen/search/SerchSection.jsx
@@ -1,12 +1,13 @@
 import TypewriterSearch from './TypewriterSearch';
 
-export default function SearchSection({ searchTerm, onSearchChange }) {
+export default function SearchSection({ searchTerm, onSearchChange, isMobile }) {
   return (
     <div className="flex-1 md:w-1/2 flex justify-center mb-8">
         <TypewriterSearch
             value={searchTerm}
             onChange={onSearchChange}
             placeholder="Пошук за назвою, автором, описом..."
+            isMobile={isMobile}
         />
     </div>
 );
